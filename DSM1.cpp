09b78a4584ec0cf1a4703b9ac8c0df6a52d3b2c9
andysosa2 <a_s350@txstate.edu>
2017-07-17 10:35:14 -0500

    Problem 6

---
+++ DSM1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+void WordPermutation(string, string);
+string substr(int startIndex, int length); 
+
+string word = "01234567";
+
+WordPermutation("",word);
+
+}
+
+void WordPermutation(string prefix, string word)
+{
+ 
+    int n = word.length();    
+    if (n == 0) { cout<< prefix <<endl; }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+  WordPermutation(prefix + word[i],word.substr(0, i) + word.substr(i + 1, n - (i+1)));
+        }
+    }
+}
