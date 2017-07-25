58a517dad95b20058036aa1ff6320840eb7d174e
Joseph Reed <reed.joseph13@gmail.com>
2017-07-25 02:29:27 -0500

    finished question 3

--- test2/question3.cpp
+++ test2/question3.cpp
@@ -23,23 +23,53 @@ int randomGen(int i) {
  	return rand()%i;
 }
 
-int main() {
-	int x, y, value;
-	cout << "input x: ";
-	cin >> x;
-	cout << "input y: ";
-	cin >> y;
+void calculate(int size, int range) {
+	int x, y, result;
+	int a[size];
 
-	value = gcd(x,y);
-
-	cout << "gcd returns: " << value << endl;
-
-	if(value > 1) {
-		cout << "x and y are not relatively prime" << endl;
-	} else {
-		cout << "x and y are relatively prime" << endl;
+	for(int i = 0; i < size; i++) {
+		
+		x = rand() % range;
+		y = rand() % range;
+		
+		result = gcd(x,y);
+		
+		if(result > 1) {
+			a[i] = 0;
+		} else {
+			a[i] = 1;
+		}
 	}
 
+	int count = 0;
+
+	for(int j = 0; j < size; j++) {
+		if(a[j] > 0) {
+			count++;
+		}
+	}
+
+	cout << "For " << size << " pairs of randomly selected integers in the range of 1 to " << range << endl;
+	double final = (double)count / size;
+	final = final * 100;
+	cout << final << "% are relatively prime.\n";
+
+	double comparison = final - 61;
+	if(comparison < 0)
+		comparison = comparison * -1;
+
+	cout << "That is " << comparison << " from the theorem's estimation (61%).\n\n";  
+
+}
+
+int main() {
+	int size = 1000;
+	int range = 1000;
+
+	for(int i = 0; i < 3; i++) {
+		calculate(size, range);
+		size = size * 10;
+	}
 	return 0;
 }
 
